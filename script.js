c7390ce67ee9e958d353f6b4ac61f3b1bcc4d61d
WILbeerT-B <wilbertbundalian@gmail.com>
2023-03-25 23:23:06 -0700

    Add UI to click buttons to get user selection
    Remove the logic for 5 rounds, add buttons and
    event listeners for each  user selection.

--- script.js
+++ script.js
@@ -11,69 +11,101 @@ function getComputerChoice() {
     } else if(randomNum == 2) {
         computerChoice = "Scissors";
     }
-    console.log(computerChoice);
+    //console.log(computerChoice);
     return computerChoice;
 }
 //play a one round game and determines the winner of the round
 function playRound(playerSelection, computerSelection) {
+    console.log(`Player: ${playerSelection} , Computer: ${computerSelection}`);
     //Player wins
-    if((playerSelection == "rock") && (computerSelection == "Scissors")){
+    if((playerSelection == "Rock") && (computerSelection == "Scissors")){
         playerCount++;
-        return "You win this round! Rock beats Scissors";
+        // return "You win this round! Rock beats Scissors";
+        console.log("You win this round! Rock beats Scissors");
         
-    } else if((playerSelection == "paper") && (computerSelection == "Rock")){
+    } else if((playerSelection == "Paper") && (computerSelection == "Rock")){
         playerCount++;
-        return "You got this round! Paper beats Rock";
+        console.log("You got this round! Paper beats Rock");
         
-    } else if((playerSelection == "scissors") && (computerSelection == "Paper")){
+    } else if((playerSelection == "Scissors") && (computerSelection == "Paper")){
         playerCount++;
-        return "You got the round! Scissors beats Paper";
+        console.log("You got the round! Scissors beats Paper");
         
     //Computer wins
-    } else if((playerSelection == "scissors") && (computerSelection == "Rock")){
+    } else if((playerSelection == "Scissors") && (computerSelection == "Rock")){
         computerCount++;
-        return "You lose this round! Rock beats Scissors!";
+        //return "You lose this round! Rock beats Scissors!";
+        console.log("You lose this round! Rock beats Scissors!");
         
-    } else if((playerSelection == "rock") && (computerSelection == "Paper")){
+    } else if((playerSelection == "Rock") && (computerSelection == "Paper")){
         computerCount++;
-        return "Computer win this round! Paper beats rock!";
+        //return "Computer win this round! Paper beats rock!";
+        console.log("Computer win this round! Paper beats rock!");
         
-    } else if((playerSelection == "paper") && (computerSelection == "Scissors")){
+    } else if((playerSelection == "Paper") && (computerSelection == "Scissors")){
         computerCount++;
-        return "You lose the round! Scissor beats paper!";
+        //return "You lose the round! Scissor beats paper!";
+        console.log("You lose the round! Scissor beats paper!");
         
     } else {
-        return "Round is EVEN!";
+        //return "Round is EVEN!";
+        console.log("Round is EVEN!");
     }
 }
 
 //get the scores of the player and computer
 function getScoreCount() {
-    return console.log(`Player score: ${playerCount}, Computer score: ${computerCount}`);
+    console.log(`SCORE: Player: ${playerCount}, Computer: ${computerCount}`);
+    if(playerCount == 5) {
+        console.log("Player wins the game!");
+        playerCount = 0;
+        computerCount = 0;
+    } else if (computerCount == 5) {
+        console.log("Computer wins the game!");
+        playerCount = 0;
+        computerCount = 0;
+    }
 }
 
 //determine the winner of the game
-function getWinner() {
-    if(playerCount > computerCount) {
-        return console.log("Player wins the game!");
-    } else if(playerCount<computerCount){
-        return console.log("Computer wins the game!");
-    } else {
-        return console.log("Game was even!")
+/* function getWinner() {
+    if(playerCount == 5) {
+        console.log("Player wins the game!");
+    } else if (computerCount == 5) {
+        console.log("Computer wins the game!");
     }
-}
+} */
 
 //call the playRound function and play a 5 round game
 function game() {
-    for(let i = 1; i <= 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        getScoreCount();
-    }
+    
+    let playerSelection = "";
+    // buttons is a node list. It looks and acts much like an array.
+    const buttons = document.querySelectorAll('button');
+    // we use the .forEach method to iterate through each button
+    //for(let i = 1; i <= 5; i++) {
+        buttons.forEach((button) => {
+
+            // and for each one we add a 'click' listener
+            button.addEventListener('click', () => {
+                playerSelection = button.id;
+                const computerSelection = getComputerChoice();
+                playRound(playerSelection, computerSelection);
+                getScoreCount();
+            });
+            
+        });
+        //const playerSelection = prompt("Rock, Paper or Scissors?");
+        
+        //console.log(playRound(playerSelection, computerSelection));
+        //const computerSelection = getComputerChoice();
+        //playRound(playerSelection, computerSelection);
+        //getScoreCount();
+    //}
+    //getWinner();
 }
 
 
 game();
-getWinner();
+
 
